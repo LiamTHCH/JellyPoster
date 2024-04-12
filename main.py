5956f6b60cf7bd2fa8853227fbb093dc0a7850fd
LiamTHCH <liam@liamtc.tech>
2024-04-12 18:18:39 +0200

    env

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from Jellyfin import JellyFin
 
-Chehtelekoum = JellyFin("http://ip","api")
+Chehtelekoum = JellyFin("https://jellyfin.chehtelekoum.de/web/index.html#!/apikeys.html","a237162d1f4446e895276dfbd02cb78f")
 
 print(Chehtelekoum.get_PLAYING_device("64.226.109.61"))
