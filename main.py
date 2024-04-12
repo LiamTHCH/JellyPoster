ed84c1585c1473d097fcaa9f0194c483192e081a
LiamTHCH <liam@liamtc.tech>
2024-04-12 18:27:06 +0200

    edit

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from Jellyfin import JellyFin
 
 Chehtelekoum = JellyFin("https://jellyfin.chehtelekoum.de/web/index.html#!/apikeys.html","a237162d1f4446e895276dfbd02cb78f")
 
-print(Chehtelekoum.get_PLAYING_device("64.226.109.61"))
+print(Chehtelekoum.get_PLAYING_device("2001:16b8:a79b:f300:9c1b:852f:fd55:b06a"))
