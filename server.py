90009b8bb30cd7cfe2537b70f174728abb2e673b
LiamTHCH <liam@liamtc.tech>
2024-04-12 19:48:12 +0200

    Update JellyFin API URL and key

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import time
 from Jellyfin import JellyFin
 
-Chehtelekoum = JellyFin("https://jellyfin.chehtelekoum.de","bacbff08a18a44c6b29f1da016f02fa0")
+Chehtelekoum = JellyFin("https://url","api")
 
 print(Chehtelekoum.get_PLAYING_device("89.247.8.169"))
 
